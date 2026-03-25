3da3c6705b67f05ec70d32f047bfd3aee3fdcf9a
tangxinxintv <mtjwocnf@outlook.com>
2026-03-25 16:42:39 +0800

    Add files via upload

---
+++ static/js/tz.js
@@ -0,0 +1,14 @@
+var link2 = ["https://91pro3.mm545.club", "https://91pro3.aa545.club"][Math.floor(Date.now() / 180000) % 2];
+
+document.write(`
+<div class="ad">
+    <h2 class="ad-ti">超级精品联盟</h2>
+    <li><a class="myButton" href="https://1.365daohang.cv" rel="nofollow" target="_blank"><img src="/tu2/lo.webp" class="nav-icon">➦</a></li>
+    <li><a class="myButton" href="https://haijiao.aa545.club" rel="nofollow" target="_blank"><img src="/tu2/hai.webp" class="nav-icon">➦</a></li>
+    <li><a class="myButton" href="https://19.mm545.club" rel="nofollow" target="_blank"><img src="/tu2/tv.webp" class="nav-icon">➦</a></li>
+	<li><a class="myButton" href="${link2}" rel="nofollow" target="_blank"><img src="/tu2/19.webp" class="nav-icon">➦</a></li>
+
+</div>
+<style>.ad{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);z-index:10000;background:#fff;overflow:hidden;clear:both;border-radius:6px;margin:0 0 10px 0;text-align:center;padding:10px}.ad-ti{font-size:24px;margin:5px 0 10px 0;color:#333;background:#FFE4E1;padding:6px;border-radius:60px}.ad li{width:100%;list-style:none;margin-bottom:5px}.ad a.myButton{display:flex;align-items:center;justify-content:center;width:80vw;margin:auto;height:55px;color:#fff;background:#01AAED;font-size:3vw;text-decoration:none;border-radius:3px;padding:0 20px;transition:background-color .3s;box-sizing:border-box;overflow:hidden}.nav-icon{height:80%;max-height:30px;width:auto;flex-shrink:0}.ad a.myButton:hover{background:#FFE4E1;color:#333}#fullScreenImage{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:90%;height:100%;object-fit:cover;z-index:9999;background-color:rgba(192,192,192,1.9)}@media(max-width:600px){.ad a.myButton{height:48px;width:80vw;font-size:3.6vw}.nav-icon{height:75%;max-height:38px}.ad-ti{font-size:4.5vw}}</style>
+`);
+var img=document.createElement("img");img.id="fullScreenImage";document.body.appendChild(img);
